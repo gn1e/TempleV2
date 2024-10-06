c0ea3aa0432c1bee08f6ffa1a99dfef14fa6fa8b
nade <88284830+gn1e@users.noreply.github.com>
2024-10-06 18:28:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const gang = require("express");
 
 console.log("backend.gang is running on port");
+
+// error 1day
+gang.use((req, res, next) => {
+    if (req.url.includes("..")) {
+        res.redirect("https://gn1e.co")
+        return;
+    }
+    error.createError(
+        "errors.com.jungle.common.not_found", 
+        "The endpoint you tried to access is not found! Report this to discord.gg/junglefn", 
+        undefined, 1004, undefined, 404, res
+    );
+});
