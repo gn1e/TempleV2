712dc272315b82feecfff72ac5d0ae107216f5b7
nade <88284830+gn1e@users.noreply.github.com>
2024-10-07 16:45:06 +0300

    yipee

---
+++ Other/log.js
@@ -0,0 +1,68 @@
+function backend() {
+    let msg = "";
+
+    for (let i in backend.arguments) {
+        msg += `${i == "0" ? "" : " "}${backend.arguments[i]}`;
+    }
+
+    console.log(`\x1b[32m[BACKEND]\x1b[0m ${msg}`);
+}
+
+function bot() {
+    let msg = "";
+
+    for (let i in bot.arguments) {
+        msg += `${i == "0" ? "" : " "}${bot.arguments[i]}`;
+    }
+
+    console.log(`\x1b[33m[DISCORD]\x1b[0m ${msg}`);
+}
+
+function xmpp() {
+    let msg = "";
+
+    for (let i in xmpp.arguments) {
+        msg += `${i == "0" ? "" : " "}${xmpp.arguments[i]}`;
+    }
+
+    console.log(`\x1b[34m[XMPP]\x1b[0m ${msg}`);
+}
+
+function error() {
+    let msg = "";
+
+    for (let i in error.arguments) {
+        msg += `${i == "0" ? "" : " "}${error.arguments[i]}`;
+    }
+
+    console.log(`\x1b[31m[ERROR]\x1b[0m ${msg}`);
+}
+
+function mongo() {
+    let msg = "";
+
+    for (let i in mongo.arguments) {
+        msg += `${i == "0" ? "" : " "}${mongo.arguments[i]}`;
+    }
+
+    console.log(`\x1b[35m[MONGO]\x1b[0m ${msg}`);
+}
+
+function endpoint() {
+    let msg = "";
+
+    for (let i in endpoint.arguments) {
+        msg += `${i == "0" ? "" : " "}${endpoint.arguments[i]}`;
+    }
+
+    console.log(`\x1b[36m[MISSING ENDPOINT]\x1b[0m ${msg}`);
+}
+
+module.exports = {
+    backend,
+    bot,
+    xmpp,
+    error,
+    mongo,
+    endpoint
+}
--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-const gang = require("express");
+const Express = require("express");
+const gang = Express();
+const path = require("path")
+const fs = require("fs")
+const log = require("./Other/log.js");
+
 
 gang.listen(3551, () => {
-    console.log(`[BACKEND] Gang gang on port 3551`);
+    log.backend(`Gang gang on port 3551`);
 }).on("error", async (err) => {
     throw err;
 });
 
-// error 1day
 gang.use((req, res, next) => {
     if (req.url.includes("..")) {
-        res.redirect("https://gn1e.co")
+        res.redirect("https://gn1e.co");
         return;
     }
-    error.createError(
-        "errors.com.jungle.common.not_found", 
-        "The endpoint you tried to access is not found! Report this to discord.gg/junglefn", 
-        undefined, 1004, undefined, 404, res
-    );
+
+    if (req.originalUrl === '/') {
+        next(); 
+        return res.status(403).send(`bomb`);
+        return;
+    }
+
+    const missiinngendpoint = req.originalUrl; 
+    const msg = `[MISSING ENDPOINT] ${missiinngendpoint}\n`; 
+    const endpointlog = path.join(__dirname, 'logs', 'missing_endpoints.log'); 
+
+    fs.appendFileSync(endpointlog, msg);
+    log.endpoint(`${missiinngendpoint}`);
+
+    var XEpicErrorName = "errors.com.nade.common.not_found";
+    var XEpicErrorCode = 1004;
+
+    res.set({
+        'X-Epic-Error-Name': XEpicErrorName,
+        'X-Epic-Error-Code': XEpicErrorCode
+    });
+
+    res.status(404).json({
+        "errorCode": XEpicErrorName,
+        "errorMessage": "Endpoint not found? Make a ticket in the support server!",
+        "numericErrorCode": XEpicErrorCode,
+        "originatingService": "any",
+        "intent": "prod"
+    });
 });
